eb5ab793a9079275ac4bf02e5476171c551b0249
Shayne Thiessen <shayne@shaynethiessen.com>
2022-06-08 08:29:03 -0500

    Update other.sh

--- configuration/other.sh
+++ configuration/other.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "Allow more file watches, needed for some programming projects (react)"
+echo "Allow more file watchers, needed for some programming projects (react)"
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
