3f59a73a1b68a6fbc943fd802142f7f12f076dac
Shayne Thiessen <shayne@shaynethiessen.com>
2022-06-22 15:10:19 -0500

    feat: updated sources

--- configuration/other.sh
+++ configuration/other.sh
@@ -2,3 +2,4 @@
 
 echo "Allow more file watchers, needed for some programming projects (react)"
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 # Install default packages
 
+sudo rm -rf /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian/ bullseye main non-free contrib" | sudo tee -a /etc/apt/sources.list
+echo "deb-src http://deb.debian.org/debian/ bullseye main non-free contrib" | sudo tee -a /etc/apt/sources.list
+echo " " | sudo tee -a /etc/apt/sources.list
+echo "deb http://security.debian.org/debian-security bullseye-security main non-free" | sudo tee -a /etc/apt/sources.list
+echo "deb-src http://security.debian.org/debian-security bullseye-security main non-free" | sudo tee -a /etc/apt/sources.list
+echo " " | sudo tee -a /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian/ bullseye-updates main non-free" | sudo tee -a /etc/apt/sources.list
+echo "deb-src http://deb.debian.org/debian/ bullseye-updates main non-free" | sudo tee -a /etc/apt/sources.list
+
 apt-get update -y;
 apt-get upgrade -y;
 DEBIAN_FRONTEND=noninteractive
