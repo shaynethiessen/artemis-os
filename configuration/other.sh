9bf95e42a7164bebf09ba7b80640cc604c411e2c
Shayne Thiessen <shayne@shaynethiessen.com>
2024-06-06 10:45:52 -0500

    Remove i386 arch support
    Closed #4

--- configuration/other.sh
+++ configuration/other.sh
@@ -2,7 +2,3 @@
 
 echo "Allow more file watchers, needed for some programming projects (react)"
 echo fs.inotify.max_user_watches=524288 | tee -a /etc/sysctl.conf && sysctl -p
-
-echo "i386 architecture support"
-dpkg --add-architecture i386
-
