00b352d3adf5e8d9ae9c266742af301a9f24ded8
Shayne Thiessen <shayne@shaynethiessen.com>
2024-06-06 10:47:36 -0500

    Removed some networking scripts
    Closed #9

--- install/networking.sh
+++ install/networking.sh
@@ -2,9 +2,3 @@
 
 echo "SSH Client"
 apt-get install --no-install-recommends -y openssh-client
-
-echo "Remote Desktop Connection client"
-apt-get install --no-install-recommends -y krdc
-
-echo "RDP client for Windows Terminal Services (X11 client)"
-apt-get install --no-install-recommends -y freerdp2-x11
